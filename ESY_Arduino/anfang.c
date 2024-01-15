b238e4b4ce109462aae2f82d5b68a18913591db7
wiktorsur <WiktorAndrzej.Surowka@htlwien10.at>
2024-01-15 14:30:06 +0100

    Neuer Code für Sensoren in cm

--- ESY_Arduino/anfang.c
+++ ESY_Arduino/anfang.c
@@ -1,29 +1,44 @@
+// Pins für die Motorsteuerung
+const int IN1 = 4;
+const int IN2 = 5;
+const int IN3 = 6;
+const int IN4 = 7;
 
-int IN1 = 4;
-int IN2 = 5;
-int IN3 = 6;
-int IN4 = 7;
+// Enable-Pins für die Motoren
+const int motorLeftEnablePin = 9;
+const int motorRightEnablePin = 10;
 
-const int sensorPinFront = A5; // Sensor-Pin am Arduino GP2Y0A02YK0F
-const int sensorPinRight = A3; // Sensor-Pin am Arduino GP2Y0A21YK0F
-const int sensorPinLeft = A4; // Sensor-Pin am Arduino GP2Y0A21YK0F
-int sensorValueFront; // Variable zur Aufnahme des Sensorwerts
-int sensorValueRight; // Variable zur Aufnahme des Sensorwerts
-int sensorValueLeft; // Variable zur Aufnahme des Sensorwerts
+// Pins für die Infrarotsensoren
+const int sensorPinRight = A3;  // Sensor-Pin am Arduino GP2Y0A02YK0F
+const int sensorPinLeft = A2;   // Sensor-Pin am Arduino GP2Y0A02YK0F
+const int sensorPinFront = A5;  // Sensor-Pin am Arduino GP2Y0A21YK0F
 
-const int startButtonPin = 2;  // Pin für den Start-Taster
-const int stopButtonPin = 3;   // Pin für den Stop-Taster
+// Start- und Stop-Taster
+const int startButtonPin = 2;
+const int stopButtonPin = 3;
 
-volatile bool isRunning = false;  // Variable zur Verfolgung des Systemzustands
+// Geschwindigkeit der Motoren (0-255)
+const int motorSpeed = 150; // Experimentiere mit diesem Wert
+
+// Sensorwerte deklarieren
+int sensorValueFront;
+int sensorValueLeft;
+int sensorValueRight;
+
+bool isRunning = false;
 
 void setup() {
+  // Initialisierung der seriellen Kommunikation
   Serial.begin(9600);
-  
+
+  // Initialisierung der Pins
   pinMode(IN1, OUTPUT);
   pinMode(IN2, OUTPUT);
   pinMode(IN3, OUTPUT);
   pinMode(IN4, OUTPUT);
-  
+  pinMode(motorLeftEnablePin, OUTPUT);
+  pinMode(motorRightEnablePin, OUTPUT);
+
   pinMode(startButtonPin, INPUT_PULLUP);
   pinMode(stopButtonPin, INPUT_PULLUP);
 
@@ -37,36 +52,24 @@ void loop() {
   sensorValueLeft = analogRead(sensorPinLeft); // Lies den analogen Wert vom Sensor
   sensorValueRight = analogRead(sensorPinRight); // Lies den analogen Wert vom Sensor
 
-  // Konvertiere den ADC-Wert in eine Spannung (0-5V entspricht 0-1023 ADC-Werten)
-  float voltageFront = sensorValueFront * (5.0 / 1023.0);
-  Serial.println(voltageFront);
-
   // Konvertiere die Spannung in den Abstand
-  float distanceFront = voltageToDistanceFront(voltageFront);
-  
-  Serial.print(sensorValueFront);
-  Serial.print("  ");
-  Serial.println(distanceFront);
-  delay(1000);
-  
+  float distanceFront = voltageToDistanceFront(sensorValueFront);
+  float distanceLeft = voltageToDistanceLeft(sensorValueLeft);
+  float distanceRight = voltageToDistanceRight(sensorValueRight);
 
+  Serial.print("Left: ");
+  Serial.print(distanceLeft);
+  Serial.print(" cm | Front:");
+  Serial.print(distanceFront);
+  Serial.print(" cm | Right:");
+  Serial.print(distanceRight);
+  Serial.println(" cm  ");
+    
   if (isRunning) {
-    if(distanceFront > 15){
-      forward();
-    }
-    else{
-      stop();
-    }
-  }
-  else{
+      forward(150, 150);
+  } else {
     stop();
   }
-}
-
-float voltageToDistanceFront(float voltageFront) {
-  float distanceFront = -49.87*pow(voltageFront,6) + 636*pow(voltageFront,5)-3345.16*pow(voltageFront,4)+9252.75*pow(voltageFront,3)-14122.49*pow(voltageFront,2)+11185.39*voltageFront-3521.13;
-  //float distanceFront = -19.98*voltageFront + 64.41;
-  return distanceFront;
 }
 
 void startButtonInterrupt() {
@@ -77,56 +80,38 @@ void stopButtonInterrupt() {
   isRunning = false;
 }
 
-void forward(){
-  analogWrite(9, 150);
-  analogWrite(10, 150);
+float voltageToDistanceLeft(float voltageLeft) {
+  float distanceLeft = 101.929921 + (-0.944760) * voltageLeft + 0.00419381697807914757 * pow(voltageLeft, 2) 
+                  - 0.00000993658241604138 * pow(voltageLeft, 3) + 0.000000011952303996626338293728 * pow(voltageLeft, 4) 
+                  - 0.000000000005721892010782867073 * pow(voltageLeft, 5);
+  return distanceLeft;
+}
+
+float voltageToDistanceFront(float voltageFront) {
+  float distanceFront = 326.520326 - 3.495438 * voltageFront + 0.01745371161904565871 * pow(voltageFront, 2)
+    - 0.00004547541709635030 * pow(voltageFront, 3)
+    + 0.000000059394041692704409989898 * pow(voltageFront, 4)
+    - 0.000000000030849183618367244279 * pow(voltageFront, 5);
+  return distanceFront;
+}
+
+float voltageToDistanceRight(float voltageRight) {
+  float distanceRight = 118.473550 + (-1.150951) * voltageRight + 0.00521565514927053390 * pow(voltageRight, 2)
+                  - 0.00001241542739366339 * pow(voltageRight, 3) + 0.000000014899084765225949588948 * pow(voltageRight, 4)
+                  - 0.000000000007097354980691055009 * pow(voltageRight, 5);
+  return distanceRight;
+}
+
+void forward(int leftSpeed, int rightSpeed) {
+  analogWrite(motorLeftEnablePin, leftSpeed);
+  analogWrite(motorRightEnablePin, rightSpeed);
   digitalWrite(IN1, HIGH);
   digitalWrite(IN2, LOW);
-  digitalWrite(IN3, LOW);
-  digitalWrite(IN4, HIGH);
+  digitalWrite(IN3, LOW);  
+  digitalWrite(IN4, HIGH);  
 }
 
-void back(){
-  analogWrite(9, 150);
-  analogWrite(10, 150);
-  digitalWrite(IN1, LOW);
-  digitalWrite(IN2, HIGH);
-  digitalWrite(IN3, HIGH);
-  digitalWrite(IN4, LOW);
-}
-
-void stop(){
-  analogWrite(9, 0);
-  analogWrite(10, 0);
-  digitalWrite(IN1, LOW);
-  digitalWrite(IN2, LOW);
-  digitalWrite(IN3, LOW);
-  digitalWrite(IN4, LOW);
-}
-
-void turnLeft() {
-  analogWrite(9, 150);
-  analogWrite(10, 0); // Reduzieren Sie das linke Rad, um nach rechts zu drehen
-  digitalWrite(IN1, HIGH);
-  digitalWrite(IN2, LOW);
-  digitalWrite(IN3, LOW);
-  digitalWrite(IN4, HIGH);
-}
-
-void turnRight() {
-  analogWrite(9, 0);
-  analogWrite(10, 150); 
-  digitalWrite(IN1, HIGH);
-  digitalWrite(IN2, LOW);
-  digitalWrite(IN3, LOW);
-  digitalWrite(IN4, HIGH);
-}
-
-void twistLeft(){
-  analogWrite(9, 150);
-  analogWrite(10, 150);
-  digitalWrite(IN1, HIGH);
-  digitalWrite(IN2, LOW);
-  digitalWrite(IN3, HIGH);
-  digitalWrite(IN4, LOW);
+void stop() {
+  analogWrite(motorLeftEnablePin, 0);
+  analogWrite(motorRightEnablePin, 0);
 }
